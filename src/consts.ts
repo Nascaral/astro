26191ce98e2870ec2ee870d8279e536fc5508f6d
Nascaral <nasser.ajobs@gmail.com>
2025-11-17 23:13:08 -0800

    Finishing up astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'My Portfolio & Blog';
+export const SITE_DESCRIPTION = 'A showcase of my web development projects, blog posts, and professional experience. Built with Astro for optimal performance.';
